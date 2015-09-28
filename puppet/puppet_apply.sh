523ec2a9972c27a028034497234683c18dbd71b6
eis <eis@iki.fi>
2015-09-28 16:40:18 +0300

    Moved to absolute path for hiera configs. Fixes #1.

--- puppet/puppet_apply.sh
+++ puppet/puppet_apply.sh
@@ -2,9 +2,6 @@
 
 basedir="$( dirname $( readlink -f "${0}" ) )"
 
-## required for the relative datadir path in hiera.yaml
-cd ${basedir}/manifests
-
 exec puppet apply \
     --hiera_config ${basedir}/manifests/hiera.yaml \
     --modulepath ${basedir}/modules \
